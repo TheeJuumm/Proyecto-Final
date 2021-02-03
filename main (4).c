e3c33a5462fb8d0952e435815261c3d4a2ee9ed9
ODVallejo <72663809+ODVallejo@users.noreply.github.com>
2021-02-03 12:01:04 -0600

    Update main (4).c
    Se logro todo el punto 1.1

--- main (4).c
+++ main (4).c
@@ -7,7 +7,7 @@ int main()
 {
     FILE *archivo;
     char nombre[50]=" ", apellidoM[50]=" ", apellidoP[50]=" ", nickname[50]=" ",
-    contrasena[50]=" ", contrasena2[50]=" ", correo[50]=" ", datos[10]=" ";
+    contrasena[50]=" ", contrasena2[50]=" ", correo[50]=" ", datos[10]=" ", cadena[100]=" ";
     char  *checkContra, *checkNick;
     int menu;
     printf("Bienvenido al sistema");
@@ -25,55 +25,69 @@ int main()
                 //Se van a preguntar y guardar los datos del nuevo usuario
                 printf("A continuacion le pedire alguno de sus datos para el registro.");
                 printf("\nCual es su nombre: ");
+                setbuf(stdin, NULL);
                 gets(nombre);
                 printf("Cual es su apellido paterno: ");
+                setbuf(stdin, NULL);
                 gets(apellidoP);
                 printf("Cual es su apellido materno: ");
+                setbuf(stdin, NULL);
                 gets(apellidoM);
                 printf("Cual es su nickname: ");
+                setbuf(stdin, NULL);
                 gets(nickname);
                 printf("Cual es su contraseña: ");
+                setbuf(stdin, NULL);
                 gets(contrasena);
                 printf("Vuelva a ingresar su contraseña: ");
+                setbuf(stdin, NULL);
                 gets(contrasena2);
                 while(strcmp(contrasena, contrasena2)!=0)//Se esta checando que las contraseñas sean iguales
                 {
                     printf("Su constraseña no se escribio igual porfavor vuelva a introducirlas.");
                     printf("\nCual es su contraseña: ");
+                    setbuf(stdin, NULL);
                     gets(contrasena);
                     printf("Vuelva a ingresar su contraseña: ");
+                    setbuf(stdin, NULL);
                     gets(contrasena2);
                 }
                 printf("Cual es su correo: ");
+                setbuf(stdin, NULL);
                 gets(correo);
                 printf("Sus datos son los siguientes;");
                 printf("\n %s ,%s ,%s ,%s ,%s ,%s  ", nombre, apellidoP, apellidoM, nickname, contrasena, correo);
                 //Se estan imprimiendo los datos y preguntando si son los correctos
                 printf("\n¿Los datos estan correctos? ");
+                setbuf(stdin, NULL);
                 gets(datos);
             }while(strcmp(datos, "no")==0);
-            /*checkNick=strstr(archivo, nickname);
-            if(strcmp(nickname, checkNick)==0)
+            do
             {
-                do
+                fgets(cadena,100,archivo);
+                checkNick=strstr(cadena, nickname);//Se checa si el usuario ya existe y si existe debe repetir el registro
+                if(checkNick!=NULL)
                 {
-                    printf("Ese usuario ya existe porfavor escoga otro: ");
-                    gets(nickname);
-                    checkNick=strstr(archivo, nickname);    
-                }while(strcmp(nickname, checkNick)==0);
-            }*/
-            fprintf(archivo, "%s: %s: %s %s %s %s \n", nickname, contrasena, nombre, apellidoP, apellidoM, correo);
-            //Se guarda primero el nickname y contraseña para tener un acceso mas facil a estos datos
-             if(fclose(archivo)==0)//Se checa si se cerro bien el archivo al guardar los datos
-            {
-                printf("Se registro con exito el usuario");
-            }
-            else
-            {
-                printf("No se pudo registrar el usuarion");
-                printf("Vuelva a intentar el registro"); //No se realizo bien el registro y se le vuelve a presentar el menu
-                printf(Menu);
-            }
+                    printf("Ese usuario ya existe");
+                    break;
+                }
+                else
+                {
+                     fprintf(archivo, "%s %s %s %s %s %s \n", nickname, contrasena, nombre, apellidoP, apellidoM, correo);
+                    //Se guarda primero el nickname y contraseña para tener un acceso mas facil a estos datos
+                    if(fclose(archivo)==0)//Se checa si se cerro bien el archivo al guardar los datos
+                        {
+                            printf("Se registro con exito el usuario");
+                            break;
+                        }
+                    else
+                        {
+                            printf("No se pudo registrar el usuarion");
+                            printf("Vuelva a intentar el registro"); //No se realizo bien el registro y se le vuelve a presentar el menu
+                            printf(Menu);
+                        }
+                }
+            }while(feof(archivo)==0);
             printf(Menu);
             scanf("%i", &menu);
         }
@@ -88,4 +102,5 @@ int main()
     }
     return 0;
 }
+
 
